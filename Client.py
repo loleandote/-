112bdd68dc9b696e539038372db2fa02fef89767
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2023-12-16 18:58:47 +0100

    list de los cojones

--- Client.py
+++ Client.py
@@ -10,25 +10,27 @@ class Client(Ice.Application):
     def run(self, argv):
         proxy = self.communicator().stringToProxy(argv[1])
         print(argv[1])
-        frontend = ServerSide.FrontendPrx.checkedCast(proxy)
+        frontend = URFS.FrontendPrx.checkedCast(proxy)
 
         if not frontend:
             raise RuntimeError('Invalid proxy')
 
         #print(frontend.write('Hello World!'))
-        #ficheros=frontend.list()
+        ficheros=frontend.getFileList()
+        for f in ficheros:
+            print(f)
         bites= str.encode("hola")
         #frontend.prueba(bites)
-        downloader=frontend.downloadFile("holawkf")
-        bytes= 1024
-        resultado=[]
-        i=0
-        while i<3:
-            resultado.append(downloader.download())
-            bytes=len(resultado)
-            i+=1
-        print(resultado)
-        downloader.destroy()
+        # downloader=frontend.downloadFile("holawkf")
+        # bytes= 1024
+        # resultado=[]
+        # i=0
+        # while i<3:
+        #     resultado.append(downloader.download())
+        #     bytes=len(resultado)
+        #     i+=1
+        # print(resultado)
+        # downloader.destroy()
         # for f in ficheros:
         #     print(f)
         return 0
--- Frontend.py
+++ Frontend.py
@@ -10,23 +10,15 @@ import URFS
 
 class FrontendI(URFS.Frontend):
     n = 0
-    def __init__(self, fileManager):
-            self.fileManager = fileManager
-    def write(self, message, current=None):
-        print("{0}: {1}".format(self.n, message))
-        sys.stdout.flush()
-        self.n += 1
-        return message
-    def list(self, current=None):
+    # def __init__(self, fileManager):
+    #         self.fileManager = fileManager
+    def getFileList(self, current=None):
         #sys.stdout.flush()
         archivos=os.listdir('./')
         lista=[]
         for archivo in archivos:
             lista.append(URFS.FileInfo(archivo,archivo))
         return lista
-    def prueba(self,baits, current=None):
-        print(len(baits))
-        sys.stdout.flush()
     def uploadFile(self, file, current=None):
         
         return self.fileManager.createUploader(file)
@@ -37,10 +29,10 @@ class Frontend(Ice.Application):
     def run(self, argv):
         broker = self.communicator()
         
-        proxy = self.communicator().stringToProxy('FileManager -t -e 1.1:tcp -h 172.28.202.67 -p 7071 -t 60000')
-        fileManager = URFS.FileManagerPrx.checkedCast(proxy)
-        servant = FrontendI(fileManager)
-
+        # proxy = self.communicator().stringToProxy('FileManager -t -e 1.1:tcp -h 172.28.202.67 -p 7071 -t 60000')
+        # fileManager = URFS.FileManagerPrx.checkedCast(proxy)
+        # servant = FrontendI(fileManager)
+        servant = FrontendI()
         adapter = broker.createObjectAdapter("FrontendAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("Frontend"))
         print(proxy, flush=True)
